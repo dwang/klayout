66de5a9a95ab3d3a68b6e329201d58be4dd89a9d
Daniel Wang <hello@danielwang.me>
2018-08-21 16:45:36 -0400

    Remove error handling in travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -3,15 +3,6 @@ set -e
 
 export PING_SLEEP=30s
 
-error_handler() {
-  echo ERROR: There was an error while building.
-  tail -500 build.txt
-  kill $PING_LOOP_PID
-  exit 1
-}
-
-trap 'error_handler' ERR
-
 bash -c "while true; do echo -n '.'; sleep $PING_SLEEP; done" &
 PING_LOOP_PID=$!
 
